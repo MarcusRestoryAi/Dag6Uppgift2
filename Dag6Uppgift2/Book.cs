d29130aa70ad367c13f291e3ad5e441fa012c9a2
Marcus <marcus.henriksson@restory.ai>
2022-10-19 11:45:50 +0200

    Add project files.

---
+++ Dag6Uppgift2/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag6Uppgift2
+{
+    internal class Book
+    {
+        //Attributes
+        public string title;
+        public int pages;
+        public static int antalBooks = 0;
+
+        //Statisk lista över Book-objekt
+        public static List<Book> books = new List<Book>();
+
+        //Konstruktor
+        public Book(string title, int pages)
+        {
+            this.title = title;
+            this.pages = pages;
+
+            //Anropar statik attribut antalBooks och ökar med 1
+            antalBooks++;
+        }
+
+        //Metod för att läsa boken
+        public void Read()
+        {
+            Console.WriteLine("Nu läser vi {0}", title);
+        }
+
+        //Statisk metod för att få en slumpmässig bok
+        public static Book RecommendBook(List<Book> listOfBooks)
+        {
+            //Skapar ett objekt av Random
+            Random rnd = new Random();
+
+            //Hämtar en slupmässig position
+            int index = rnd.Next(listOfBooks.Count);
+
+            //Hämtar en random book från listan
+            Book book = listOfBooks[index];
+
+            //Returnera book
+            return book;
+
+            // return listOfBooks[index];
+        }
+    }
+}
---
+++ Dag6Uppgift2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Dag6Uppgift2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa ett antal böcker
+            //Spara dem i statisk lista inuti Book klassen
+            Book.books.Add(new Book("Lord of the Rings", 300));
+            Book.books.Add(new Book("Harry Potter", 400));
+            Book.books.Add(new Book("Mordet på Orientexpressen", 350));
+            Book.books.Add(new Book("Lord of the Flies", 700));
+            Book.books.Add(new Book("Mitt liv som Bert", 700));
+
+            //ForEach loop för att läsa varje book samt skriva ut info
+            foreach (Book book in Book.books)
+            {
+                Console.WriteLine("{0} har {1} sidor.", book.title, book.pages);
+                book.Read();
+            }
+
+            //Skriv ut hur många böcker som har blivit skapta
+            Console.WriteLine("Det finns sammanlagt {0} antal böcker!", Book.antalBooks);
+
+            //Bli rekommenderad en slumpmässig book
+            Book recommendedBook = Book.RecommendBook(Book.books);
+
+            //Skriv ut titel av rekommenderad book
+            Console.WriteLine("Nu skall jag läsa {0} igen!", recommendedBook.title);
+        }
+    }
+}
